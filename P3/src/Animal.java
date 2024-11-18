9bd2f1a3105238a84aa81ccd9dcaf9644749b844
DavidUMAGitHub <davidmunvalle@gmail.com>
2024-11-18 11:44:10 +0100

    Cree clases

---
+++ P3/src/Animal.java
@@ -0,0 +1,11 @@
+
+public class Animal {
+	
+	private string estadoAnimal;	// Lo de los enumerados no se como va ¿Se crea una clase?
+	private date nacimiento; 
+	
+	public Animal( string estadoAnimal, date nacimiento ) {
+		this.estadoAnimal = estadoAnimal;
+		this.nacimiento = nacimiento;
+	}
+}
